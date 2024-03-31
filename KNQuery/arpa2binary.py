4da2891ae67395736e3c3db32de8ad860a1e0bf8
Patrick Wierzbicki <59071826+pw45000@users.noreply.github.com>
2024-03-31 19:52:59 -0400

    Experimental Sharding for arpa2binary.py
    Even with 30 GB of memory, the original script runs out of memory. A temporary fix in the form of saving each separate element of the list is put in place, as the original list is something in the form of 
    a list with 5 elements with 5 sublists, each representing unigram, bigrams, etc..

--- KNQuery/arpa2binary.py
+++ KNQuery/arpa2binary.py
@@ -1,8 +1,14 @@
+import json
+
+import numpy
+import numpy as np
 import torch
 import logging
 import argparse
 from tqdm import tqdm
+import pickle
 from utils import load_tokenizer
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -44,6 +50,18 @@ def replace_special_tokens(ws, eos_token, unk_token):
     return ws
 
 
+def save(data, sidx):
+    logger.info(f"Saving the {sidx}-gram...")
+    file_path = f"{sidx}gram.json"
+    data_np = numpy.array(data, dtype="object")
+    np.save(file=f"{sidx}gram.npy", arr = data_np, allow_pickle = True)
+    logger.info(f"{sidx}-gram saved. ")
+
+# VERY DANGEROUS! Taken from https://stackoverflow.com/questions/12417498/how-to-release-used-memory-immediately-in-python-list.
+def release_list(a):
+   del a[:]
+   del a
+
 def main(args):
     replace = not args.no_replace
     if args.tokenizer_path:
@@ -74,11 +92,16 @@ def main(args):
                 if "gram" in line:
                     sidx += 1
                     if sidx == 0:
+                        logger.info("Loading {}-gram...".format(sidx + 1))
                         data[sidx] += read_special_tokens(fin, tokenizer, args.eos_token)
-                    logger.info("Loading {}-gram...".format(sidx+1))
+                    if sidx != 0:
+                        save(data, sidx)
+                        release_list(data)
+                        data = [[], [], [], [], []]
+                        logger.info("Loading {}-gram...".format(sidx + 1))
                 continue
             it = line.strip().split("\t")
-            if len(it) > 2: 
+            if len(it) > 2:
                 p, ws, b = float(it[0]), it[1], float(it[2])
                 ws = replace_special_tokens(ws, args.eos_token, args.unk_token) if replace else ws
                 data[sidx].append((p, tokenizer(ws), b))
@@ -86,10 +109,11 @@ def main(args):
                 p, ws = float(it[0]), it[1]
                 ws = replace_special_tokens(ws, args.eos_token, args.unk_token) if replace else ws
                 data[sidx].append((p, tokenizer(ws)))
-
-    logger.info("Saving...")
-    torch.save(data, args.binary)
-
-
+        # For whatever reason, it tried saving this as 4-gram when it was 5-gram and overwrote the original 4-gram.
+        # Hacked fix for now by adding one to it.
+        save(data, sidx+1)
+        release_list(data)
+        data = [[], [], [], [], []]
+        logger.info("Saved all n-grams.")
 if __name__ == "__main__":
     main(parse_args())
